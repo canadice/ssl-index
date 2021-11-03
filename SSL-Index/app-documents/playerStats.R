07a6737214a0715fd870d1e0d67b129d0ca104eb
Isak Hietala <isahi126@gmail.com>
2021-11-03 11:48:26 +0100

    Updated view to reflect the SSL.

--- SSL-Index/app-documents/playerStats.R
+++ SSL-Index/app-documents/playerStats.R
@@ -18,7 +18,7 @@ playerStatsUI <- function(id){
     fluidPage(
       fluidRow(
         column(
-          width = 2,
+          width = 10,
           offset = 1,
           DTOutput(outputId = ns("playerStats"))
         )
@@ -35,19 +35,128 @@ playerStatsSERVER <- function(id){
     
     ## Definining the mechanisms
     function(input, output, session){
-     
-      url <- "https://raw.githack.com/canadice/ssl-index/main/SSL-Index/data/sttas.html"
+
+      ###  Reading the data
+           
+      url <- "https://raw.githack.com/canadice/ssl-index/main/SSL-Index/data/playerStats.html"
+      
+      ###  Processing the data                                         
       
       playerStats <- 
         url %>% 
         read_html() %>% 
         html_elements("table") %>% 
-        html_table()
+        html_table() %>% 
+        .[[1]] %>% 
+        rename(
+          Information = `Inf`,
+          Appearances = Apps,
+          `Minutes per Game` = `Mins/Gm`,
+          Goals = Gls,
+          Assists = Ast,
+          `Completed Passes` = `Ps C`,
+          `Pass%` = `Pas %`,
+          `Shots on Target` = ShT,
+          `Tackles Won` = `Tck W`,
+          `Tackles Lost` = `Tck A`,
+          Dribbles = `Drb`,
+          `Completed Crosses` = `Cr C`,
+          `Completed Attempted` = `Cr A`,
+          Headers = Hdrs,
+          `Headers%` = `Hdr %`,
+          Interceptions = Itc,
+          Fouls = Fls,
+          `Distance Run (mi)` = Distance,
+          `Average Rating` = `Av Rat`,
+          `Player of the Match` = `PoM`
+        ) %>% 
+        mutate(
+          Position = NA,
+          Nationality = 
+            Name %>% 
+            str_split(
+              pattern = " - ", 
+              simplify = TRUE
+            ) %>% 
+            .[,2],
+          Name = 
+            Name %>% 
+            str_split(
+              pattern = " - ", 
+              simplify = TRUE
+            ) %>% 
+            .[,1],
+          `Pass%` = 
+            `Pass%` %>% 
+            str_remove_all(
+              pattern = "%"
+            ) %>% 
+            as.numeric()/100,
+          `Headers%` =
+            `Headers%` %>% 
+            str_remove_all(
+              pattern = "%"
+            ) %>% 
+            as.numeric()/100,
+          `Distance Run (mi)` = 
+            `Distance Run (mi)` %>% 
+            str_remove_all(
+              pattern = "mi"
+            ) %>% 
+            as.numeric()
+        ) %>% 
+        mutate(
+          across(
+            !contains(
+              c("Name", "Information", "Nationality")
+            ),
+            as.numeric
+          )
+        ) %>% 
+        relocate(
+          c(
+            Nationality,
+            Position,
+            Appearances,
+            `Minutes per Game`,
+            `Distance Run (mi)`
+          ),
+          .after = Name
+        ) %>% 
+        select(
+          -Information
+        ) %>% 
+        # As there are non-numeric values being transformed correctly to NA, warnings are suppressed. 
+        suppressWarnings()
+        
       
       output$playerStats <- renderDT({
-        playerStats[[1]]
-      })
-       
+        if(length(playerStats) == 0){
+          NULL
+        } else {
+          playerStats  
+        }
+      },
+      class = 'compact cell-border stripe',
+      rownames = FALSE,
+      escape = FALSE,
+      options = 
+        list(
+          ordering = TRUE, 
+          ## Sets a scroller for the rows
+          scrollY = '80%',
+          sScrollX = "100%",
+          ## Sets size of rows shown
+          scrollCollapse = TRUE,
+          pageLength = 10,
+          dom = 'Rftp',
+          ## Sets color of table background
+          initComplete = JS(
+            "function(settings, json) {",
+            "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff'});",
+            "}")
+        )
+      )
     }
   )
 }
