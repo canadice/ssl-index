e7c219d39eb3fd9bf1bbbf0192599f2fe36945f4
Canadice <canadice.shl@gmail.com>
2022-06-01 15:10:37 +0200

    Test run of the scraperJob for GitHub Actions.

--- scrapers/scraperJob.R
+++ scrapers/scraperJob.R
@@ -25,7 +25,7 @@ require(dbplyr)
 require(RSQLite)
 
 ## Opens the connection to the SQLite Database
-con <- dbConnect(SQLite(), "../database/SSL_Database.db")
+con <- dbConnect(SQLite(), "database/SSL_Database.db")
 
 
 #################################################################
@@ -42,7 +42,7 @@ playerLinks <-
   unname() %>% 
   unique()
 
-playerData <- 
+forumData <- 
   lapply(
     playerLinks,
     FUN = function(x){
@@ -86,9 +86,68 @@ playerData <-
   dplyr::relocate(
     c(lastPost, Active),
     .after = `All Traits`
+  ) %>% 
+  dplyr::mutate(
+    across(
+      .cols = `TPE Available`:`Throwing`,
+      as.numeric
+    ),
+    `Natural Fitness` = 20,
+    Stamina = 20,
+    Goalkeeper = if_else(Position == "Goalkeeper", 20, NaN),
+    Name = paste(`First Name`, `Last Name`) %>% str_squish(),
+    
+    `Minimum Wage` = 
+      case_when(
+        TPE <= 350 ~ 1*10^6,
+        TPE <= 500 ~ 1.5*10^6,
+        TPE <= 650 ~ 2*10^6,
+        TPE <= 800 ~ 2.5*10^6,
+        TPE <= 950 ~ 3*10^6,
+        TPE <= 1100 ~ 3.5*10^6,
+        TPE <= 1250 ~ 4*10^6,
+        TPE <= 1400 ~ 4.5*10^6,
+        TPE <= 1550 ~ 5*10^6,
+        TPE <= 1750 ~ 5.5*10^6,
+        TRUE ~ 6*10^6
+      )
+  ) %>% 
+  relocate(
+    Goalkeeper,
+    .after = `Defense [R]`
+  ) %>% 
+  relocate(
+    Name, 
+    .after = `Last Name`
+  ) %>% 
+  relocate(
+    Striker:Goalkeeper,
+    .after = `Preferred Position`
+  ) %>% 
+  dplyr::mutate(
+    Position = ifelse(is.na(Position), "Undefined", Position),
+    Position = 
+      factor(Position, levels = c("Goalkeeper", "Defender", "Midfielder", "Forward", "Undefined")),
+    across(
+      .cols = Striker:Goalkeeper,
+      as.numeric
+    )
+  ) %>% 
+  arrange(
+    Created
+  ) %>% 
+  relocate(
+    `Minimum Wage`,
+    .after = `All Traits`
+  ) %>% 
+  select(
+    Username:Active
   )
 
+write.csv(forumData, file = "data/forumData.csv", row.names = FALSE)
 
+dbWriteTable(con, "Daily_Scrape", forumData, overwrite = TRUE)
 
+dbDisconnect(con)
 
 
