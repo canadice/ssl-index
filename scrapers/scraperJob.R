9d571ee32b0e0627cab887b79bd4752d9eccbc65
luketd <lukedamato99@gmail.com>
2022-06-01 22:22:31 -0400

    Fixed scraperJob.R by adding db call to Team Information

--- scrapers/scraperJob.R
+++ scrapers/scraperJob.R
@@ -27,6 +27,8 @@ require(RSQLite)
 ## Opens the connection to the SQLite Database
 con <- RSQLite::dbConnect(RSQLite::SQLite(), "database/SSL_Database.db")
 
+teamInfo <- 
+  dbGetQuery(con, "SELECT * FROM Team_Information")
 
 #################################################################
 ##                      Scrapes the forum                      ##
@@ -57,45 +59,9 @@ forumData <-
       } 
       else {
         scrape
-      }  
+      }
     }
-  )
-
-playerLinks <-
-  topic %>%
-  rvest::html_elements(".topic-row") %>%
-  rvest::html_elements(".row4 a") %>%
-  rvest::html_attr("href") %>%
-  .[
-    stringr::str_detect(string = ., pattern = "simsoccer")
-  ]
-
-
-
-try <- forumData %>%
-  do.call(
-  what = plyr::rbind.fill,
-  args = .
-)
-
-forumData <- 
-  lapply(
-    playerLinks,
-    FUN = function(x){
-      scrape <- try(playerScraper(x), silent=TRUE)
-      
-      if( 
-        inherits(
-          scrape,  
-          "try-error")
-      ){
-        print(x)
-      } 
-      else {
-        scrape
-      }  
-    }
-  ) %>% 
+  )%>% 
   do.call(
     what = plyr::rbind.fill,
     args = .
