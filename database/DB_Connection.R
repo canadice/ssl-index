3d783da233a45a70a86620d44a7f2b47ce51a18c
luketd <lukedamato99@gmail.com>
2022-06-01 21:21:38 -0400

    Added queries for career stats stuff

--- database/DB_Connection.R
+++ database/DB_Connection.R
@@ -20,6 +20,294 @@ dbListTables(con)
 
 
 #################################################################
+##              Set up Queries for other files                    ##
+#################################################################
+
+PlayerDataRegularSeason <- dbGetQuery(con, 
+"SELECT 
+Name,
+sum(`Minutes Played`) as `Minutes Played` ,
+sum(`Distance Run (km)`) as `Distance Run (km)` ,
+avg(`Average Rating`) as `Average Rating` ,
+sum(`Player of the Match`) as `Player of the Match` ,
+sum(`Goals`) as Goals ,
+sum(`Assists`) as Assists ,
+sum(`xG`) as xG ,
+sum(`Shots on Target`) as `Shots on Target` ,
+sum(`Shots`) as Shots ,
+sum(`Penalties Taken`) as `Penalties Taken` ,
+sum(`Penalties Scored`) as `Penalties Scored` ,
+sum(`Successful Passes`) as `Successful Passes` ,
+sum(`Attempted Passes`) as `Attempted Passes` ,
+avg(`Pass%`) as `Pass%` ,
+sum(`Key Passes`) as `Key Passes` ,
+sum(`Successful Crosses`) as `Successful Crosses` ,
+sum(`Attempted Crosses`) as `Attempted Crosses` ,
+avg(`Cross%`) as `Cross%` ,
+sum(`Chances Created`) as `Chances Created` ,
+sum(`Successful Headers`) as `Successful Headers` ,
+sum(`Attempted Headers`) as `Attempted Headers` ,
+avg(`Header%`) as `Header%` ,
+sum(`Key Headers`) as `Key Headers` ,
+sum(`Dribbles`) as Dribbles ,
+sum(`Tackles Won`) as `Tackles Won` ,
+sum(`Attempted Tackles`) as `Attempted Tackles` ,
+avg(`Tackle%`) as `Tackle%` ,
+sum(`Key Tackles`) as `Key Tackles` ,
+sum(`Interceptions`) as Interceptions ,
+sum(`Clearances`) as Clearances ,
+sum(`Mistakes Leading to Goals`) as `Mistakes Leading to Goals` ,
+sum(`Yellow Cards`) as `Yellow Cards` ,
+sum(`Red Cards`) as `Red Cards` ,
+sum(`Fouls`) as Fouls ,
+sum(`Fouls Against`) as `Fouls Against` ,
+sum(`Offsides`) as Offsides,
+count(Matchday) as GamesPlaye
+  FROM Player_Game_Data  group by Name, Season"
+)
+
+
+
+PlayeRDataPlayoffQuery <- 
+'SELECT 
+Name,
+sum(`Minutes Played`) as `Minutes Played` ,
+sum(`Distance Run (km)`) as `Distance Run (km)` ,
+avg(`Average Rating`) as `Average Rating` ,
+sum(`Player of the Match`) as `Player of the Match` ,
+sum(`Goals`) as Goals ,
+sum(`Assists`) as Assists ,
+sum(`xG`) as xG ,
+sum(`Shots on Target`) as `Shots on Target` ,
+sum(`Shots`) as Shots ,
+sum(`Penalties Taken`) as `Penalties Taken` ,
+sum(`Penalties Scored`) as `Penalties Scored` ,
+sum(`Successful Passes`) as `Successful Passes` ,
+sum(`Attempted Passes`) as `Attempted Passes` ,
+avg(`Pass%`) as `Pass%` ,
+sum(`Key Passes`) as `Key Passes` ,
+sum(`Successful Crosses`) as `Successful Crosses` ,
+sum(`Attempted Crosses`) as `Attempted Crosses` ,
+avg(`Cross%`) as `Cross%` ,
+sum(`Chances Created`) as `Chances Created` ,
+sum(`Successful Headers`) as `Successful Headers` ,
+sum(`Attempted Headers`) as `Attempted Headers` ,
+avg(`Header%`) as `Header%` ,
+sum(`Key Headers`) as `Key Headers` ,
+sum(`Dribbles`) as Dribbles ,
+sum(`Tackles Won`) as `Tackles Won` ,
+sum(`Attempted Tackles`) as `Attempted Tackles` ,
+avg(`Tackle%`) as `Tackle%` ,
+sum(`Key Tackles`) as `Key Tackles` ,
+sum(`Interceptions`) as Interceptions ,
+sum(`Clearances`) as Clearances ,
+sum(`Mistakes Leading to Goals`) as `Mistakes Leading to Goals` ,
+sum(`Yellow Cards`) as `Yellow Cards` ,
+sum(`Red Cards`) as `Red Cards` ,
+sum(`Fouls`) as Fouls ,
+sum(`Fouls Against`) as `Fouls Against` ,
+sum(`Offsides`) as Offsides,
+count(Matchday) as GamesPlaye
+  FROM Player_Game_Data  where MatchDay LIKE  "%Cup%" group by Name, Season'
+
+PlayeRDataPlayoff <- dbGetQuery(con,PlayeRDataPlayoffQuery)
+
+
+PlayerSumRegular <- dbGetQuery(con, 
+                               "SELECT 
+Name,
+sum(`Minutes Played`) as `Minutes Played` ,
+sum(`Distance Run (km)`) as `Distance Run (km)` ,
+avg(`Average Rating`) as `Average Rating` ,
+sum(`Player of the Match`) as `Player of the Match` ,
+sum(`Goals`) as Goals ,
+sum(`Assists`) as Assists ,
+sum(`xG`) as xG ,
+sum(`Shots on Target`) as `Shots on Target` ,
+sum(`Shots`) as Shots ,
+sum(`Penalties Taken`) as `Penalties Taken` ,
+sum(`Penalties Scored`) as `Penalties Scored` ,
+sum(`Successful Passes`) as `Successful Passes` ,
+sum(`Attempted Passes`) as `Attempted Passes` ,
+avg(`Pass%`) as `Pass%` ,
+sum(`Key Passes`) as `Key Passes` ,
+sum(`Successful Crosses`) as `Successful Crosses` ,
+sum(`Attempted Crosses`) as `Attempted Crosses` ,
+avg(`Cross%`) as `Cross%` ,
+sum(`Chances Created`) as `Chances Created` ,
+sum(`Successful Headers`) as `Successful Headers` ,
+sum(`Attempted Headers`) as `Attempted Headers` ,
+avg(`Header%`) as `Header%` ,
+sum(`Key Headers`) as `Key Headers` ,
+sum(`Dribbles`) as Dribbles ,
+sum(`Tackles Won`) as `Tackles Won` ,
+sum(`Attempted Tackles`) as `Attempted Tackles` ,
+avg(`Tackle%`) as `Tackle%` ,
+sum(`Key Tackles`) as `Key Tackles` ,
+sum(`Interceptions`) as Interceptions ,
+sum(`Clearances`) as Clearances ,
+sum(`Mistakes Leading to Goals`) as `Mistakes Leading to Goals` ,
+sum(`Yellow Cards`) as `Yellow Cards` ,
+sum(`Red Cards`) as `Red Cards` ,
+sum(`Fouls`) as Fouls ,
+sum(`Fouls Against`) as `Fouls Against` ,
+sum(`Offsides`) as Offsides,
+count(Matchday) as GamesPlaye
+  FROM Player_Game_Data  group by Name"
+)
+                               
+     
+PlayerSumPlayoff <- dbGetQuery(con, 
+                               "SELECT 
+Name,
+sum(`Minutes Played`) as `Minutes Played` ,
+sum(`Distance Run (km)`) as `Distance Run (km)` ,
+avg(`Average Rating`) as `Average Rating` ,
+sum(`Player of the Match`) as `Player of the Match` ,
+sum(`Goals`) as Goals ,
+sum(`Assists`) as Assists ,
+sum(`xG`) as xG ,
+sum(`Shots on Target`) as `Shots on Target` ,
+sum(`Shots`) as Shots ,
+sum(`Penalties Taken`) as `Penalties Taken` ,
+sum(`Penalties Scored`) as `Penalties Scored` ,
+sum(`Successful Passes`) as `Successful Passes` ,
+sum(`Attempted Passes`) as `Attempted Passes` ,
+avg(`Pass%`) as `Pass%` ,
+sum(`Key Passes`) as `Key Passes` ,
+sum(`Successful Crosses`) as `Successful Crosses` ,
+sum(`Attempted Crosses`) as `Attempted Crosses` ,
+avg(`Cross%`) as `Cross%` ,
+sum(`Chances Created`) as `Chances Created` ,
+sum(`Successful Headers`) as `Successful Headers` ,
+sum(`Attempted Headers`) as `Attempted Headers` ,
+avg(`Header%`) as `Header%` ,
+sum(`Key Headers`) as `Key Headers` ,
+sum(`Dribbles`) as Dribbles ,
+sum(`Tackles Won`) as `Tackles Won` ,
+sum(`Attempted Tackles`) as `Attempted Tackles` ,
+avg(`Tackle%`) as `Tackle%` ,
+sum(`Key Tackles`) as `Key Tackles` ,
+sum(`Interceptions`) as Interceptions ,
+sum(`Clearances`) as Clearances ,
+sum(`Mistakes Leading to Goals`) as `Mistakes Leading to Goals` ,
+sum(`Yellow Cards`) as `Yellow Cards` ,
+sum(`Red Cards`) as `Red Cards` ,
+sum(`Fouls`) as Fouls ,
+sum(`Fouls Against`) as `Fouls Against` ,
+sum(`Offsides`) as Offsides,
+count(Matchday) as GamesPlaye
+  FROM Player_Game_Data  where MatchDay LIKE  \"%Cup%\" group by Name"
+)                          
+
+
+KeeperDataRegularSeason <-dbGetQuery(con,
+                                     "SELECT 
+                                     Name,
+                                     sum(`Minutes Played`) as `Minutes Played` ,
+                                     avg(`Average Rating`) as `Average Rating` ,
+                                     sum(`Player of the Match`) as `Player of the Match` ,
+                                     sum(`Won`) as Won ,
+                                     sum(`Lost`) as Lost ,
+                                     sum(`Drawn`) as Drawn ,
+                                     sum(`Clean Sheets`) as `Clean Sheets` ,
+                                     sum(`Conceded`) as Conceded ,
+                                     sum(`Saves Parried`) as `Saves Parried` ,
+                                     sum(`Saves Held`) as `Saves Held` ,
+                                     sum(`Saves Tipped`) as `Saves Tipped` ,
+                                     avg(`Save%`) as `Save%` ,
+                                     sum(`Penalties Faced`) as `Penalties Faced` ,
+                                     sum(`Penalties Saved`) as `Penalties Saved` ,
+                                     avg(`xSave%`) as `xSave%` ,
+                                     sum(`Result`) as Result ,
+                                     sum(`Opponent`) as Opponent ,
+                                     sum(`Matchday`) as Matchday ,
+                                     count(`Season`) as GamesPlayed 
+                                     FROM Keeper_Game_Data group by Name, Season"                                
+                                     
+)
+
+
+KeeperDataPlayoff <-dbGetQuery(con,
+                                     "SELECT 
+                                     Name,
+                                     sum(`Minutes Played`) as `Minutes Played` ,
+                                     avg(`Average Rating`) as `Average Rating` ,
+                                     sum(`Player of the Match`) as `Player of the Match` ,
+                                     sum(`Won`) as Won ,
+                                     sum(`Lost`) as Lost ,
+                                     sum(`Drawn`) as Drawn ,
+                                     sum(`Clean Sheets`) as `Clean Sheets` ,
+                                     sum(`Conceded`) as Conceded ,
+                                     sum(`Saves Parried`) as `Saves Parried` ,
+                                     sum(`Saves Held`) as `Saves Held` ,
+                                     sum(`Saves Tipped`) as `Saves Tipped` ,
+                                     avg(`Save%`) as `Save%` ,
+                                     sum(`Penalties Faced`) as `Penalties Faced` ,
+                                     sum(`Penalties Saved`) as `Penalties Saved` ,
+                                     avg(`xSave%`) as `xSave%` ,
+                                     sum(`Result`) as Result ,
+                                     sum(`Opponent`) as Opponent ,
+                                     sum(`Matchday`) as Matchday ,
+                                     count(`Season`) as GamesPlayed 
+                                     FROM Keeper_Game_Data  where  MatchDay like \"%Cup%\" group by Name, Season"                                
+                                     
+)
+
+KeeperSumPlayoff <-dbGetQuery(con,
+                               "SELECT 
+                                     Name,
+                                     sum(`Minutes Played`) as `Minutes Played` ,
+                                     avg(`Average Rating`) as `Average Rating` ,
+                                     sum(`Player of the Match`) as `Player of the Match` ,
+                                     sum(`Won`) as Won ,
+                                     sum(`Lost`) as Lost ,
+                                     sum(`Drawn`) as Drawn ,
+                                     sum(`Clean Sheets`) as `Clean Sheets` ,
+                                     sum(`Conceded`) as Conceded ,
+                                     sum(`Saves Parried`) as `Saves Parried` ,
+                                     sum(`Saves Held`) as `Saves Held` ,
+                                     sum(`Saves Tipped`) as `Saves Tipped` ,
+                                     avg(`Save%`) as `Save%` ,
+                                     sum(`Penalties Faced`) as `Penalties Faced` ,
+                                     sum(`Penalties Saved`) as `Penalties Saved` ,
+                                     avg(`xSave%`) as `xSave%` ,
+                                     sum(`Result`) as Result ,
+                                     sum(`Opponent`) as Opponent ,
+                                     sum(`Matchday`) as Matchday ,
+                                     count(`Season`) as GamesPlayed 
+                                     FROM Keeper_Game_Data where  MatchDay like \"%Cup%\" group by Name"                                
+                               
+)
+
+KeeperSumRegular <-dbGetQuery(con,
+                              "SELECT 
+                                     Name,
+                                     sum(`Minutes Played`) as `Minutes Played` ,
+                                     avg(`Average Rating`) as `Average Rating` ,
+                                     sum(`Player of the Match`) as `Player of the Match` ,
+                                     sum(`Won`) as Won ,
+                                     sum(`Lost`) as Lost ,
+                                     sum(`Drawn`) as Drawn ,
+                                     sum(`Clean Sheets`) as `Clean Sheets` ,
+                                     sum(`Conceded`) as Conceded ,
+                                     sum(`Saves Parried`) as `Saves Parried` ,
+                                     sum(`Saves Held`) as `Saves Held` ,
+                                     sum(`Saves Tipped`) as `Saves Tipped` ,
+                                     avg(`Save%`) as `Save%` ,
+                                     sum(`Penalties Faced`) as `Penalties Faced` ,
+                                     sum(`Penalties Saved`) as `Penalties Saved` ,
+                                     avg(`xSave%`) as `xSave%` ,
+                                     sum(`Result`) as Result ,
+                                     sum(`Opponent`) as Opponent ,
+                                     sum(`Matchday`) as Matchday ,
+                                     count(`Season`) as GamesPlayed 
+                                     FROM Keeper_Game_Data group by Name"                                
+                              
+)
+
+
+#################################################################
 ##              Insert Daily Scrape into DB                    ##
 #################################################################
 
