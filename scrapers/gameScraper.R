09c0b0ba99338b63485f27cb3ca5ff396db3d56d
Canadice <canadice.shl@gmail.com>
2022-07-06 12:51:37 +0200

    MD17 and SF L1

--- scrapers/gameScraper.R
+++ scrapers/gameScraper.R
@@ -732,7 +732,7 @@ outfieldOutput <- function(season, matchday){
 
 season <- 4
 
-date <- "2022-02-12" %>% as.Date()
+date <- "2022-03-04" %>% as.Date()
 
 ## Adding a deauthorization for reading of Google Sheets that are still being used. 
 googlesheets4::gs4_deauth()
