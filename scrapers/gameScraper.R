b6c79fc45b9faf7bf6ce118993ed46d38a0e9980
Canadice <canadice.shl@gmail.com>
2022-07-26 21:40:21 +0200

    S5 Completed with all matches.

--- scrapers/gameScraper.R
+++ scrapers/gameScraper.R
@@ -732,7 +732,7 @@ outfieldOutput <- function(season, matchday){
 
 season <- 4
 
-date <- "2022-04-09" %>% as.Date()
+date <- "2022-05-18" %>% as.Date()
 
 ## Adding a deauthorization for reading of Google Sheets that are still being used. 
 googlesheets4::gs4_deauth()
