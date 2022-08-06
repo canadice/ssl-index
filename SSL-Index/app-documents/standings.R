81fd48029d4e4f54e4a2c572a190c9a692e33860
Canadice <canadice.shl@gmail.com>
2022-08-06 11:08:55 +0200

    Update to S5 with two divisions.

--- SSL-Index/app-documents/standings.R
+++ SSL-Index/app-documents/standings.R
@@ -32,7 +32,11 @@ standingsUI <- function(id){
         column(
           width = 10,
           offset = 1,
-          reactableOutput(outputId = ns("standings"))
+          reactableOutput(outputId = ns("standings1")),
+          br(),
+          uiOutput(
+            outputId = ns("standings2UI")
+          )
         )
       )
     )
@@ -48,12 +52,23 @@ standingsSERVER <- function(id){
     ## Definining the mechanisms
     function(input, output, session){
       
-      standings <- 
+      standings1 <- 
         reactive({
-          read_sheet(
-            ss = "https://docs.google.com/spreadsheets/d/1jcsFLjtiq-jK273DI-m-N38x9yUS66HwuX5x5Uig8Uc/edit?usp=sharing", 
-            sheet = paste("Season", input$season)
-          ) %>% 
+          sheet <- 
+            read_sheet(
+              ss = "https://docs.google.com/spreadsheets/d/1jcsFLjtiq-jK273DI-m-N38x9yUS66HwuX5x5Uig8Uc/edit?usp=sharing", 
+              sheet = paste("Season", input$season)
+            ) 
+          
+          if(input$season > 4){
+            sheet <- 
+              sheet %>% 
+              filter(
+                Division == 1
+              ) 
+          }
+            
+          sheet %>% 
             mutate(
               `In-game Date` = `In-game Date` %>% as_date() %>% format(format = "%m/%d"),
               `IRL Date` = `IRL Date` %>% as_date() %>% format(format = "%m/%d"),
@@ -163,9 +178,141 @@ standingsSERVER <- function(id){
             )
         })
       
-      output$standings <- renderReactable({
+      standings2 <- 
+        reactive({
+          sheet <- 
+            read_sheet(
+              ss = "https://docs.google.com/spreadsheets/d/1jcsFLjtiq-jK273DI-m-N38x9yUS66HwuX5x5Uig8Uc/edit?usp=sharing", 
+              sheet = paste("Season", input$season)
+            ) 
+          
+          if(input$season > 4){
+            sheet <- 
+              sheet %>% 
+              filter(
+                Division == 2
+              ) 
+            
+            if(all(sheet$Result %>% is.na())){
+              sheet <- NULL
+            } else {
+              sheet %>% 
+                mutate(
+                  `In-game Date` = `In-game Date` %>% as_date() %>% format(format = "%m/%d"),
+                  `IRL Date` = `IRL Date` %>% as_date() %>% format(format = "%m/%d"),
+                  HomeScore = 
+                  stringr::str_split(
+                    Result, 
+                    pattern = "-", 
+                    simplify = TRUE
+                  )[,1], 
+                  AwayScore = 
+                  stringr::str_split(
+                    Result, 
+                    pattern = "-", 
+                    simplify = TRUE
+                  )[,2]
+                ) %>%
+                mutate(
+                  HomePoints = 
+                  case_when(
+                    str_detect(HomeScore, "e|p") ~ 3,
+                    HomeScore > AwayScore ~ 3,
+                    HomeScore == AwayScore ~ 1,
+                    TRUE ~ 0
+                  ),
+                  AwayPoints = 
+                  case_when(
+                    str_detect(AwayScore, "e|p") ~ 3,
+                    HomeScore < AwayScore ~ 3,
+                    HomeScore == AwayScore ~ 1,
+                    TRUE ~ 0
+                  )
+                ) %>% 
+                select(
+                  -`IRL Date`,
+                  -`In-game Date`,
+                  -Result
+                ) %>% 
+                pivot_longer(
+                  c(HomePoints, AwayPoints),
+                  names_to = c("set", ".value"),
+                  names_pattern = "(....)(.*)$"
+                ) %>% 
+                mutate(
+                  Team = 
+                  case_when(
+                    set == "Home" ~ Home,
+                    TRUE ~ Away
+                  ),
+                  GF = 
+                  case_when(
+                    set == "Home" ~ HomeScore,
+                    TRUE ~ AwayScore
+                  ),
+                  GA = 
+                  case_when(
+                    set == "Home" ~ AwayScore,
+                    TRUE ~ HomeScore
+                  ),
+                  Matchday = unlist(Matchday)
+                ) %>% 
+                select(
+                  -contains("Home"),
+                  -contains("Away"),
+                  -set
+                ) %>% 
+                filter(
+                  !is.na(GF) & GF != "",
+                  !is.na(as.numeric(Matchday) %>% suppressWarnings()) 
+                ) %>% 
+                group_by(
+                  Team
+                ) %>% 
+                summarize(
+                  W = sum(Points == 3),
+                  D = sum(Points == 1),
+                  L = sum(Points == 0),
+                  GF = sum(as.numeric(GF)),
+                  GA = sum(as.numeric(GA)),
+                  GD = GF-GA,
+                  Points = sum(Points)
+                ) %>% 
+                arrange(
+                  desc(Points),
+                  desc(GD)
+                ) %>% 
+                ungroup() %>% 
+                mutate(
+                  Pos = 1:n()
+                ) %>% 
+                left_join(
+                  teamInfo %>% 
+                  select(
+                    team, 
+                    color_primary,
+                    color_secondary,
+                    logo
+                  ),
+                  by = c("Team" = "team")
+                ) %>% 
+                relocate(
+                  c(Pos, logo), 
+                  .before = Team
+                ) %>% 
+                rename(
+                  ` ` = logo,
+                  Pts = Points
+                )
+            } 
+          } else {
+            sheet <- NULL
+          }
+        })
+      
+      output$standings1 <- renderReactable({
         reactable(
-          standings() %>% 
+          standings1() %>% 
             select(
               -contains("color")
             ), 
@@ -178,9 +325,9 @@ standingsSERVER <- function(id){
               style = function(value, index){
                 list(
                   background = 
-                    standings()$color_primary[index],
+                    standings1()$color_primary[index],
                   color = 
-                    standings()$color_secondary[index]
+                    standings1()$color_secondary[index]
                   )
               }
             ),
@@ -210,9 +357,76 @@ standingsSERVER <- function(id){
                     function(value, index){
                       list(
                         background = 
-                          standings()$color_primary[index],
+                          standings1()$color_primary[index],
                         color = 
-                          standings()$color_secondary[index],
+                          standings1()$color_secondary[index],
+                        fontWeight = "bold"
+                      )
+                    }
+                )
+            )
+        ) 
+      })
+      
+      
+      output$standings2UI <- renderUI({
+        if(is.null(standings2())){
+          NULL
+        } else {
+          reactableOutput(outputId = ns("standings2"))
+        }
+      })
+      
+      output$standings2 <- renderReactable({
+        reactable(
+          standings2() %>% 
+            select(
+              -contains("color")
+            ),
+          pagination = FALSE,
+          fullWidth = FALSE,
+          defaultColDef = 
+            colDef(
+              maxWidth = 60,
+              align = "center",
+              style = function(value, index){
+                list(
+                  background = 
+                    standings2()$color_primary[index],
+                  color = 
+                    standings2()$color_secondary[index]
+                )
+              }
+            ),
+          columns = 
+            list(
+              ` ` = 
+                colDef(
+                  width = 50,
+                  align = "center",
+                  class = "cell",
+                  cell = function(value){
+                    logo <- 
+                      img(
+                        class = "logo",
+                        src = value,
+                        height = 30
+                      )
+                    
+                    div(class = "club", logo)
+                  }
+                ),
+              Team = 
+                colDef(
+                  width = 150,
+                  align = "left",
+                  style = 
+                    function(value, index){
+                      list(
+                        background = 
+                          standings2()$color_primary[index],
+                        color = 
+                          standings2()$color_secondary[index],
                         fontWeight = "bold"
                       )
                     }
