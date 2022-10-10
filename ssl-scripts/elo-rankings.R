341c7b1c510b8f1be70f10922fad71fd6078a308
Canadice <isahi126@gmail.com>
2022-10-10 14:31:19 +0200

    ELO rankings script

---
+++ ssl-scripts/elo-rankings.R
@@ -0,0 +1,217 @@
+source("D:/GitHubs/ssl-index/SSL-Index/app-documents/dataLoader.R")
+require(googlesheets4)
+require(lubridate)
+require(tidyr)
+require(ggplot2)
+require(stringr)
+require(plotly)
+
+schedule <- 
+  read_sheet(
+    ss = "https://docs.google.com/spreadsheets/d/1jcsFLjtiq-jK273DI-m-N38x9yUS66HwuX5x5Uig8Uc/edit?usp=sharing", 
+    sheet = paste("Season", 5)
+  ) %>% 
+  filter(
+    !(str_detect(Matchday, "Pre")|str_detect(Matchday, "Shield"))
+  ) %>% 
+  mutate(
+    `In-game Date` = `In-game Date` %>% as_date() %>% format(format = "%m/%d"),
+    `IRL Date` = `IRL Date` %>% as_date() %>% format(format = "%m/%d"),
+    HomeScore = 
+      stringr::str_split(
+        Result, 
+        pattern = "-", 
+        simplify = TRUE
+      )[,1], 
+    AwayScore = 
+      stringr::str_split(
+        Result, 
+        pattern = "-", 
+        simplify = TRUE
+      )[,2]
+  ) %>%
+  mutate(
+    HomePoints = 
+      case_when(
+        str_detect(HomeScore, "e|p") ~ 3,
+        HomeScore > AwayScore ~ 3,
+        HomeScore == AwayScore ~ 1,
+        TRUE ~ 0
+      ),
+    AwayPoints = 
+      case_when(
+        str_detect(AwayScore, "e|p") ~ 3,
+        HomeScore < AwayScore ~ 3,
+        HomeScore == AwayScore ~ 1,
+        TRUE ~ 0
+      )
+  ) %>% 
+  select(
+    -`IRL Date`,
+    -`In-game Date`,
+    -Result
+  )
+
+# elo <- teamInfo %>%
+#   select(
+#     team
+#   ) %>%
+#   filter(
+#     team != "FA"
+#   ) %>%
+#   mutate(
+#     time = 0,
+#     ELO = 1500
+#   )
+
+cur_schedule <- 
+  schedule %>% 
+  mutate(
+    Matchday = 
+      unlist(Matchday) %>%
+      factor(levels = unique(.)) %>% 
+      as.numeric() + max(elo$time)
+  )
+
+for(i in 1:nrow(cur_schedule)){
+  cur_rank <- 
+    elo %>%
+    filter(
+      team %in% c(cur_schedule$Home[i], cur_schedule$Away[i])
+    ) %>% 
+    group_by(
+      team
+    ) %>% 
+    filter(
+      time == max(time)
+    ) %>% 
+    ungroup() %>% 
+    mutate(
+      team = 
+        factor(team, 
+               levels = 
+                 c(cur_schedule$Home[i], 
+                   cur_schedule$Away[i]))
+    ) %>% 
+    arrange(
+      team
+    ) %>% 
+    select(
+      ELO
+    ) %>% 
+    unlist()
+  
+  ## Calculating the expected score
+  Q <- 10^(cur_rank/400)
+  
+  E <- Q / sum(Q)
+  
+  ## Calculating the observed score
+  S <- cur_schedule[i,c("HomePoints", "AwayPoints")] / 
+    sum(cur_schedule[i,c("HomePoints", "AwayPoints")])
+  
+  ## Calculates new rank
+  K <- NA
+  
+  if(cur_rank[1] < 2100){
+    K[1]  <- 32 
+  } else if (cur_rank[1] > 2400){
+    K[1] <- 16
+  } else {
+    K[1] <- 24
+  }
+  
+  if(cur_rank[2] < 2100){
+    K[2]  <- 32 
+  } else if (cur_rank[2] > 2400){
+    K[2] <- 16
+  } else {
+    K[2] <- 24
+  }
+  
+  new_rank <- (cur_rank + K * (S - E)) %>% unname()
+  
+  ## Summarizes the new data
+  new_data <- 
+    data.frame(
+      team = cur_schedule$Home[i],
+      time = cur_schedule$Matchday[i],
+      ELO = new_rank[1]
+    ) %>% 
+    rbind(
+      c(
+        team = cur_schedule$Away[i],
+        time = cur_schedule$Matchday[i],
+        ELO = new_rank[2]
+      )
+    )
+  
+  elo <- 
+    elo %>% 
+    rbind(
+      new_data
+    )
+}
+
+
+p <- 
+  ggplot(data = elo) + aes(x = time, y = ELO, color = team) + 
+  geom_line(size = 1) + theme_bw() +
+  scale_color_manual(
+    "Team",
+    values = 
+      teamInfo %>% 
+      filter(
+        team != "FA"
+      ) %>% 
+      arrange(
+        team
+      ) %>% 
+      select(
+        color_primary
+      ) %>% 
+      unlist() %>% 
+      unname()
+  ) +
+  labs(
+    x = "Time"
+  ) + 
+  geom_vline(
+    xintercept = c(59, 15, 34, 88)
+  ) + 
+  scale_x_continuous(
+    breaks = NULL
+  ) + 
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank()
+  ) 
+
+p %>% 
+  ggplotly()
+
+elo %>% 
+  group_by(team) %>% 
+  filter(time == max(time), team != "FC Rio") %>% 
+  ungroup() %>% 
+  arrange(desc(ELO)) %>% 
+  select(-time) %>% 
+  mutate(
+    ELO = round(ELO, 0)
+  ) %>% 
+  rename(Team = team) %>% 
+  reactable::reactable(
+    pagination = FALSE,
+    fullWidth = FALSE,
+    columns = 
+      list(
+        Team = 
+          reactable::colDef(
+            width = 150
+          )  
+      )
+    )
+  
+
+
+
