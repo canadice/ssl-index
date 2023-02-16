e912992a5e597ab1063d24390f327f49c78aa459
Canadice <isahi126@gmail.com>
2023-02-16 09:48:13 +0100

    Updated to Cup Group Game 1

--- scrapers/gameScraper.R
+++ scrapers/gameScraper.R
@@ -774,7 +774,7 @@ outfieldOutput <- function(season, matchday){
 
 season <- 8
 
-date <- "2022-08-09" %>% as.Date()
+date <- "2022-08-29" %>% as.Date()
 
 {
   ## Adding a deauthorization for reading of Google Sheets that are still being used. 
