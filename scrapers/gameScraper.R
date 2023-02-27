aff564bb85a853d9c3d2457eb6ae47da3f3dbdbf
Canadice <isahi126@gmail.com>
2023-02-27 20:01:33 +0100

    Updated to D2 MD5

--- scrapers/gameScraper.R
+++ scrapers/gameScraper.R
@@ -774,7 +774,7 @@ outfieldOutput <- function(season, matchday){
 
 season <- 8
 
-date <- "2022-09-19" %>% as.Date()
+date <- "2022-11-01" %>% as.Date()
 
 {
   ## Adding a deauthorization for reading of Google Sheets that are still being used. 
