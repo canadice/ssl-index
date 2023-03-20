7e48b0c5668f5794931948056e0e4fed540415ed
Canadice <isahi126@gmail.com>
2023-03-20 18:20:22 +0100

    Updated to Cup Semi Final

--- scrapers/gameScraper.R
+++ scrapers/gameScraper.R
@@ -774,7 +774,7 @@ outfieldOutput <- function(season, matchday){
 
 season <- 8
 
-date <- "2022-02-14" %>% as.Date()
+date <- "2022-03-24" %>% as.Date()
 
 {
   ## Adding a deauthorization for reading of Google Sheets that are still being used. 
