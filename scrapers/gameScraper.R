1117486df702a72d611bd2786ab4b8ddad0753df
Canadice <isahi126@gmail.com>
2023-05-27 13:25:29 +0200

    Updated D2MD14

--- scrapers/gameScraper.R
+++ scrapers/gameScraper.R
@@ -774,7 +774,7 @@ outfieldOutput <- function(season, matchday){
 
 season <- 9
 
-date <- "2023-04-10" %>% as.Date()
+date <- "2023-05-08" %>% as.Date()
 
 {
   ## Adding a deauthorization for reading of Google Sheets that are still being used. 
