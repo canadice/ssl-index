4a59c15239ce1f578b7e8451bbc394c7d696539d
Canadice <isahi126@gmail.com>
2023-06-17 13:18:57 +0200

    Updates for scraping nationalities

--- scrapers/scraperJob.R
+++ scrapers/scraperJob.R
@@ -30,6 +30,41 @@ con <- RSQLite::dbConnect(RSQLite::SQLite(), "database/SSL_Database.db")
 teamInfo <- 
   dbGetQuery(con, "SELECT * FROM Team_Information")
 
+countries <- c(
+  "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda",
+  "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain",
+  "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan",
+  "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria",
+  "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada",
+  "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros",
+  "Democratic Republic of the Congo", "Republic of the Congo", "Costa Rica", 
+  "Croatia", "Cuba", "Cyprus", "Czechia", "Denmark",
+  "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt",
+  "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia",
+  "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana",
+  "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti",
+  "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland",
+  "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati",
+  "Korea, North", "Korea, South", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia",
+  "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania",
+  "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta",
+  "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova",
+  "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia",
+  "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria",
+  "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Panama",
+  "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal",
+  "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia",
+  "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe",
+  "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore",
+  "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan",
+  "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan",
+  "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago",
+  "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine",
+  "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan",
+  "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe",
+  "England", "Scotland", "Wales", "Northern Ireland", "USA", "Czech Republic"
+)
+
 #################################################################
 ##                      Scrapes the forum                      ##
 #################################################################
@@ -113,7 +148,23 @@ forumData <-
         TPE <= 1750 ~ 5.5*10^6,
         TRUE ~ 6*10^6
       ),
-    `All Traits` = `All Traits` %>% str_to_title()
+    `All Traits` = `All Traits` %>% str_to_title(),
+    Nationality = 
+      stringr::str_extract(
+        Birthplace, 
+        pattern = 
+          paste0(
+            countries %>% 
+              stringr::str_trim(), 
+            collapse = "|"
+          ) 
+      ) %>% 
+      dplyr::if_else(
+        . == "USA", "United States", .
+      ) %>% 
+      dplyr::if_else(
+        . == "Czech Republic", "Czechia", .
+      )
   ) %>% 
   relocate(
     Goalkeeper,
