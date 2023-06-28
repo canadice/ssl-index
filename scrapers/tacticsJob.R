3d2ffbf7be563ce58333201944a90d93d229c5be
Canadice <isahi126@gmail.com>
2023-06-28 08:22:21 +0200

    Adding tactics deadline reminder.

---
+++ scrapers/tacticsJob.R
@@ -0,0 +1,73 @@
+
+###########################################################################
+###########################################################################
+###                                                                     ###
+###               TACTICS DEADLINE REMINDER FOR GITHUB ACTIONS               ###
+###                                                                     ###
+###########################################################################
+###########################################################################
+
+#remotes::install_github("Canadice/sslrtools")
+# remotes::install_github("EriqLaplus/discordr")
+require(discordr)
+require(sslrtools)
+
+require(rvest)
+
+require(plyr)
+
+require(dplyr)
+
+require(tidyr)
+
+require(stringr)
+
+require(lubridate)
+
+hooks <- 
+  c(
+    Sys.getenv('TACTICSACR'),
+    Sys.getenv('TACTICSATH'),
+    Sys.getenv('TACTICSCABA'),
+    Sys.getenv('TACTICSCAT'),
+    Sys.getenv('TACTICSCAI'),
+    Sys.getenv('TACTICSFCK'),
+    Sys.getenv('TACTICSHOL'),
+    Sys.getenv('TACTICSLAO'),
+    Sys.getenv('TACTICSLON'),
+    Sys.getenv('TACTICSMTL'),
+    Sys.getenv('TACTICSPAR'),
+    Sys.getenv('TACTICSREY'),
+    Sys.getenv('TACTICSSEO'),
+    Sys.getenv('TACTICSSFV'),
+    Sys.getenv('TACTICSTOK'),
+    Sys.getenv('TACTICSUSP')
+  )
+
+sendDeadline <- function(x){
+  conn_obj <- 
+    create_discord_connection(
+      webhook = x, 
+      username = 'Tactics Reminder', 
+      set_default = TRUE)
+  
+  send_webhook_message(
+    paste(
+      "=======================================",
+      "Tactics deadline has passed!", 
+      "======================================="
+    )
+  )
+}
+
+
+lapply(
+  X = hooks, 
+  FUN = sendDeadline
+)
+
+
+
+
+
+
