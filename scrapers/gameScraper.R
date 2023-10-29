e0310ec0f786df76f508d5651626bb09091491d3
Canadice <isahi126@gmail.com>
2023-10-29 16:59:02 +0100

    Minors MD3

--- scrapers/gameScraper.R
+++ scrapers/gameScraper.R
@@ -818,7 +818,7 @@ outfieldOutput <- function(season, matchday){
 
 season <- 12
 
-date <- "2023-08-08" %>% as.Date()
+date <- "2023-10-03" %>% as.Date()
 
 {
   ## Adding a deauthorization for reading of Google Sheets that are still being used. 
