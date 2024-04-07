039c554dbad6a7027ce859b906bb713c67a1c7cc
Canadice <isahi126@gmail.com>
2024-04-07 19:55:19 +0200

    Adding different sized spinners for correct placeholder sizing

---
+++ SSL-Index/app-documents/functions/input/spinners.R
@@ -0,0 +1,8 @@
+withSpinnerSmall <- function(x) {
+  withSpinner(ui_element = x, proxy.height = "20px")
+}
+
+withSpinnerMedium <- function(x) {
+  withSpinner(ui_element = x, proxy.height = "100px")
+}
+
