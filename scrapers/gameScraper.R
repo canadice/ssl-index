22cfb53bcdb0659c1f8accf1312cb79e7ee1ef15
Canadice <isahi126@gmail.com>
2024-06-10 19:08:59 +0200

    MIN MD6

--- scrapers/gameScraper.R
+++ scrapers/gameScraper.R
@@ -831,7 +831,7 @@ outfieldOutput <- function(season, matchday){
 
 season <- "15"
 
-date <- "2024-04-02" %>% as.Date()
+date <- "2024-05-28" %>% as.Date()
 
 {
   ## Adding a deauthorization for reading of Google Sheets that are still being used. 
