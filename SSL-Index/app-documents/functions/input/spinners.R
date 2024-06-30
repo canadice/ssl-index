a96b3c3769588ef3b8c484e83c2377fa618b4a9a
Canadice <isahi126@gmail.com>
2024-06-30 14:13:08 +0200

    Changing spinner type and size

--- SSL-Index/app-documents/functions/input/spinners.R
+++ SSL-Index/app-documents/functions/input/spinners.R
@@ -1,12 +1,12 @@
 withSpinnerSmall <- function(x) {
-  withSpinner(ui_element = x, proxy.height = "20px")
+  withSpinner(ui_element = x, proxy.height = "20px", type = 8, size = 0.5)
 }
 
 withSpinner30 <- function(x) {
-  withSpinner(ui_element = x, proxy.height = "30px")
+  withSpinner(ui_element = x, proxy.height = "30px", type = 8, size = 0.5)
 }
 
 withSpinnerMedium <- function(x) {
-  withSpinner(ui_element = x, proxy.height = "100px")
+  withSpinner(ui_element = x, proxy.height = "100px", type = 8, size = 0.5)
 }
 
