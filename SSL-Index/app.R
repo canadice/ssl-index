0273fa707e2328823733ef7d987c21da24f01b7e
Kenny Gilles <kenny.gilles@gmail.com>
2024-11-09 15:35:16 -0500

    Create custom flex tags

--- SSL-Index/app.R
+++ SSL-Index/app.R
@@ -196,6 +196,13 @@ sapply(
 ##                  The UI and Server function                  ##
 ##################################################################
 
+# Custom tags
+flexCol <<- function(cont, style) {
+  shiny::tag("div", varArgs = list(cont, class = "flex-col-wrapper", style = style))
+}
+flexRow <<- function(cont, style) {
+  shiny::tag("div", varArgs = list(cont, class = "flex-row-wrapper", style = style))
+}
 
 ui <- function(request){
   
