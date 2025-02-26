9244b31a2b0a2d366236b0a27174cb5ddc682c53
Canadice <isahi126@gmail.com>
2025-02-26 20:51:46 +0100

    Adding dependencies

--- SSL-Index/dependencies.R
+++ SSL-Index/dependencies.R
@@ -1,7 +1,9 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(box)
+library(bslib)
 library(plotly)
 library(rhino)
+library(shiny.router)
 library(tidyverse)
 library(treesitter)
 library(treesitter.r)
