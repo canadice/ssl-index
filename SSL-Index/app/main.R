f28a8171d7213b4ff8a1a317d26d85f0a2556ffb
Canadice <isahi126@gmail.com>
2025-02-26 20:52:05 +0100

    Adding SSL css and routing

--- SSL-Index/app/main.R
+++ SSL-Index/app/main.R
@@ -1,22 +1,36 @@
 box::use(
-  shiny[bootstrapPage, div, moduleServer, NS, renderUI, tags, uiOutput],
+  shiny,
+  bslib,
+  sass,
+  shiny.router[router_ui, router_server, route, route_link],
 )
 
 box::use(
   app/view/welcome,
 )
 
+theme <- bslib$bs_theme(primary = "purple") |>
+  bslib$bs_add_rules(sass$sass_file("app/styles/main.scss"))
+
 #' @export
 ui <- function(id) {
-  ns <- NS(id)
-  bootstrapPage(
-    welcome$ui(ns("message"))
+  ns <- shiny$NS(id)
+  bslib$page_fillable(
+    title = "SSL Portal",
+    # sidebar = "SIDEBAR",
+    theme = theme,
+    router_ui(
+      route("welcome", welcome$ui(ns("message")))
+    )
+    
   )
 }
 
 #' @export
 server <- function(id) {
-  moduleServer(id, function(input, output, session) {
+  shiny$moduleServer(id, function(input, output, session) {
+    router_server("welcome")
+    
     welcome$server("message", usergroup = 1)
   })
 }
