df87279a57b16bcca6481029a675f67a604334a5
Canadice <isahi126@gmail.com>
2025-03-04 17:11:00 +0100

    Adding tpe plotly

--- SSL-Index/app/view/tracker/player.R
+++ SSL-Index/app/view/tracker/player.R
@@ -1,12 +1,14 @@
 box::use(
   bslib,
   dplyr,
-  plotly[plotlyOutput, renderPlotly, plot_ly, config],
+  lubridate[as_date, floor_date, today],
+  plotly,
   promises[future_promise, then],
   reactable[colDef, colFormat, reactable, reactableOutput, renderReactable],
   shiny,
   shiny.router[get_query_param, change_page],
-  stringr[str_to_upper],
+  stringr[str_remove, str_split, str_to_upper],
+  tidyr[complete, pivot_longer],
   tippy[tippy],
 )
 
@@ -27,32 +29,31 @@ ui <- function(id) {
   shiny$tagList(
     bslib$card(
       bslib$card_header(
-        bslib$layout_columns(
-          col_widths = c(2, 4, 6),
+        bslib$layout_column_wrap(
+          width = NULL,
+          style = bslib$css(grid_template_columns = "1fr 2fr 3fr"),
           shiny$uiOutput(ns("selectPlayer")),
           shiny$uiOutput(ns("filterPlayer")),
           ""
         )
       ),
       bslib$card_body(
-        bslib$layout_columns(
-          col_widths = c(6, 6, 8, 4, 12),
-          fillable = FALSE,
-          # Col 6
+        bslib$layout_column_wrap(
+          width = 1/2,
           bslib$card(
             bslib$card_header(
               shiny$h3("Profile Information")
             ),
             bslib$card_body(
-              bslib$layout_columns(
-                col_widths = c(9, 3, 12),
+              bslib$layout_column_wrap(
+                width = NULL,
+                style = bslib$css(grid_template_columns = "3fr 1fr"),
                 shiny$uiOutput(ns("playerName")),
-                shiny$uiOutput(ns("clubLogo"), height = NULL),
-                shiny$uiOutput(ns("playerInfo"))
-              )
+                shiny$uiOutput(ns("clubLogo"), height = NULL)
+              ),
+              shiny$uiOutput(ns("playerInfo"))
             )
           ),
-          # Col 6
           bslib$card(
             bslib$card_header(
               shiny$h3("Recent Match Statistics")
@@ -60,8 +61,11 @@ ui <- function(id) {
             bslib$card_body(
               reactableOutput(ns("matchStatistics"))
             )
-          ),
-          # Col 8
+          )
+        ),
+        bslib$layout_column_wrap(
+          width = NULL,
+          style = bslib$css(grid_template_columns = "2fr 1fr"),
           bslib$card(
             bslib$card_header(
               shiny$h3("Player Attributes")
@@ -69,24 +73,22 @@ ui <- function(id) {
             bslib$card_body(
               shiny$uiOutput(ns("playerAttributes"))
             )
-          ), 
-          # Col 4
+          ),
           bslib$card(
             bslib$card_header(
               shiny$h3("Player Attributes")
             ),
             bslib$card_body(
-              plotlyOutput(ns("tpeProgression"))
+              plotly$plotlyOutput(ns("tpeProgression"))
             )
+          )
+        ),
+        bslib$card(
+          bslib$card_header(
+            shiny$h3("Player History")
           ),
-          # Col 12...
-          bslib$card(
-            bslib$card_header(
-              shiny$h3("Player History")
-            ),
-            bslib$card_body(
-              shiny$uiOutput(ns("playerHistory"))
-            )
+          bslib$card_body(
+            shiny$uiOutput(ns("playerHistory"))
           )
         )
       )
@@ -225,21 +227,163 @@ server <- function(id) {
             })
             
             output[["playerInfo"]] <- shiny$renderUI({
+              value <- 
+                data |> 
+                dplyr$select(contains("pos_")) |> 
+                pivot_longer(everything()) |> 
+                dplyr$mutate(
+                  name = str_remove(name, pattern = "pos_") |> 
+                    str_to_upper()
+                )
+              
               shiny$tagList(
                 bslib$layout_columns(
                   col_widths = c(6, 6),
-                  
+                  shiny$tagList(
+                    shiny$h4(paste("TPE: ", data$tpe)),
+                    shiny$h4(paste("Banked TPE: ", data$tpebank)),
+                    # shiny$h4(paste("Bank Balance: ", data$bank)),
+                    shiny$h4(paste("Player Status: "), data$playerStatus, class = data$playerStatus),
+                    shiny$h4(paste("User Status: "), data$userStatus, class = data$userStatus),
+                    shiny$h5(paste("Nationality:"), data$nationality), 
+                    shiny$h5(paste("Render: "), data$render)
+                  ),
+                  shiny$tagList(
+                    shiny$h4("Traits"),
+                    data$traits |> 
+                      str_split(pattern = constant$traitSep) |> 
+                      unlist() |> 
+                      paste(collapse = "<br>") |> 
+                      shiny$HTML(),
+                    shiny$br(),
+                    shiny$h4("Primary Position(s)"),
+                    value |> 
+                      dplyr$filter(value == 20) |> 
+                      dplyr$select(name) |> 
+                      unlist() |> 
+                      paste(collapse = ", ") |> 
+                      shiny$HTML(),
+                    shiny$h4("Secondary Position(s)"),
+                    value |> 
+                      dplyr$filter(value < 20, value >= 10) |> 
+                      dplyr$select(name) |> 
+                      unlist() |>
+                      paste(collapse = ", ") |>
+                      shiny$HTML()
+                  )
                 )
               )
             })
             
+            output[["tpeProgression"]] <- plotly$renderPlotly({
+              historyTPE() |> 
+                then(
+                  onFulfilled = function(tpe){
+                    if(nrow(tpe) < 2){
+                      plotly$plot_ly() |>
+                        plotly$add_annotations(
+                          text = "The player has had no TPE<br>progression in the Portal",
+                          x = 0.5, y = 0.5,
+                          xref = "paper", yref = "paper",
+                          showarrow = FALSE,
+                          font = list(size = 20),
+                          align = "center",
+                          borderpad = 10,
+                          bgcolor = "rgba(255, 255, 255, 0.5)"
+                        ) |>
+                        plotly$layout(
+                          xaxis = list(showgrid = FALSE, zeroline = FALSE, showline = FALSE, showticklabels = FALSE),
+                          yaxis = list(showgrid = FALSE, zeroline = FALSE, showline = FALSE, showticklabels = FALSE),
+                          margin = list(l = 0, r = 0, b = 0, t = 0),
+                          plot_bgcolor = "#333333",   # background color
+                          paper_bgcolor = "#333333"
+                        ) |> 
+                        plotly$config(
+                          displayModeBar = TRUE,  # Enable display of mode bar (optional, true by default)
+                          modeBarButtonsToRemove = list(
+                            "toImage", "zoom2d", "pan2d", "select2d",
+                            "lasso2d", "zoomIn2d", "zoomOut2d",
+                            "autoScale2d", "resetScale2d"
+                          ),
+                          displaylogo = FALSE  # Remove Plotly logo
+                        )
+                    } else {
+                      visData <- 
+                        tpe |> 
+                        dplyr$mutate(
+                          WeekStart = 
+                            floor_date(Time |> 
+                                         as_date(), 
+                                       "week",
+                                       week_start = 1)
+                          ) |> 
+                        dplyr$group_by(WeekStart) |> 
+                        dplyr$summarize(total = sum(`TPE Change`, na.rm = TRUE)) |> 
+                        complete(
+                          WeekStart = 
+                            seq(
+                              min(WeekStart), 
+                              floor_date(today() |> 
+                                           as_date(tz = "US/Pacific"),
+                                         "week", 
+                                         week_start = 1),
+                              by = "week"
+                              ),
+                          fill = list(total = 0)
+                          ) |> 
+                        dplyr$ungroup() |> 
+                        dplyr$mutate(cumulative = cumsum(total), 
+                                     week = 1:dplyr$n()) |> 
+                        suppressMessages()
+                      
+                      plotly$plot_ly(visData, hoverinfo = "text") |> 
+                        plotly$add_trace(x = ~week, y = ~cumulative, type = 'scatter', mode = 'markers+lines',
+                                  line = list(color = constant$sslGold),
+                                  marker = list(size = 5, color = constant$sslGold),
+                                  text = ~paste("Week:", week, "<br>TPE:", cumulative)
+                        ) |> 
+                        plotly$layout(
+                          title = list(
+                            text = "TPE Progression",
+                            font = list(color = "white")  # Set title text color to white
+                          ),
+                          xaxis = list(
+                            title = "Time",
+                            tickfont = list(color = "white"),  # Set x-axis tick labels color to white
+                            titlefont = list(color = "white"),  # Set x-axis title color to white
+                            dtick = 1,
+                            showgrid = FALSE
+                          ),
+                          yaxis = list(
+                            title = "TPE",
+                            range = c(300, 2100),
+                            tickfont = list(color = "white"),  # Set y-axis tick labels color to white
+                            titlefont = list(color = "white"),  # Set y-axis title color to white
+                            dtick = 200,  # Show tickmarks at intervals of 200
+                            gridcolor = "rgba(255, 255, 255, 0.5)",  # Set gridline color to white with opacity
+                            gridwidth = 1  # Set gridline width
+                          ),
+                          plot_bgcolor = "#333333",   # background color
+                          paper_bgcolor = "#333333",   # plot area background color
+                          showlegend = FALSE  # Hide legend (optional)
+                        ) |> 
+                        plotly$config(
+                          displayModeBar = TRUE,  # Enable display of mode bar (optional, true by default)
+                          modeBarButtonsToRemove = list(
+                            "zoom2d", "pan2d", "select2d",
+                            "lasso2d", "zoomIn2d", "zoomOut2d",
+                            "autoScale2d", "resetScale2d"
+                          ),
+                          displaylogo = FALSE  # Remove Plotly logo
+                        )
+                    }
+                  }
+                )
+            })
           }
         )
     }) |> 
       shiny$bindEvent(input$selectedPlayer)
-    
-    
-    
     
     
     shiny$observe({
