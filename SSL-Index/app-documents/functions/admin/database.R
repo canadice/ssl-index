104b53b941cd0666d64a88e1f619404253fc6718
Canadice <isahi126@gmail.com>
2025-04-28 07:13:31 +0200

    Adding a trycatch to always disconnect connection

--- SSL-Index/app-documents/functions/admin/database.R
+++ SSL-Index/app-documents/functions/admin/database.R
@@ -1,65 +1,56 @@
 ## Loads config information for database information
 config <- config::get(config = "mysql")
 
+query <- function(query, schema){
+  
+  tryCatch({
+    con <- 
+      dbConnect(
+        MySQL(),
+        dbname = schema,
+        host = config$mysql$host,
+        port = config$mysql$port,
+        user = config$mysql$user,
+        password = config$mysql$pass
+      )
+    
+    dbSendQuery(con, "SET NAMES utf8mb4;")
+    dbSendQuery(con, "SET CHARACTER SET utf8mb4;")
+    dbSendQuery(con, "SET character_set_connection=utf8mb4;")
+    
+    req <- glue::glue_sql(query, .con = con)
+    
+    # print(req)
+    
+    req <- dbSendQuery(con, req)
+    res <- dbFetch(req, n = -1)
+    
+    dbClearResult(req)
+    
+    return(res)
+  }, error = function(e) {
+    # Log or handle the error
+    message("Error executing query: ", e$message)
+    return(NULL)
+  }, finally = {
+    # Ensure the connection is closed
+    if (!is.null(con) && dbIsValid(con)) {
+      dbDisconnect(con)
+    }
+  })
+}
+
 ## Function for queries to mybb
 mybbQuery <- function(query){
   
-  con <- 
-    dbConnect(
-      MySQL(),
-      dbname = config$mysql$mybb,
-      host = config$mysql$host,
-      port = config$mysql$port,
-      user = config$mysql$user,
-      password = config$mysql$pass
-    )
-  
-  dbSendQuery(con, "SET NAMES utf8mb4;")
-  dbSendQuery(con, "SET CHARACTER SET utf8mb4;")
-  dbSendQuery(con, "SET character_set_connection=utf8mb4;")
-  
-  req <- glue::glue_sql(query, .con = con)
-  
-  req <- dbSendQuery(con, req)
-  res <- dbFetch(req, n = -1)
-  
-  dbClearResult(req)
-  
-  dbDisconnect(con)
-  
-  return(res)
+  query(query, schema = config$mysql$mybb)
   
 }
 
 ## Function for queries to portal
 portalQuery <- function(query){
   
-  con <- 
-    dbConnect(
-      MySQL(),
-      dbname = config$mysql$portal,
-      host = config$mysql$host,
-      port = config$mysql$port,
-      user = config$mysql$user,
-      password = config$mysql$pass
-    )
-  
-  dbSendQuery(con, "SET NAMES utf8mb4;")
-  dbSendQuery(con, "SET CHARACTER SET utf8mb4;")
-  dbSendQuery(con, "SET character_set_connection=utf8mb4;")
-  
-  req <- glue::glue_sql(query, .con = con)
-  
-  # print(req)
-  
-  req <- dbSendQuery(con, req)
-  res <- dbFetch(req, n = -1)
-  
-  dbClearResult(req)
-  
-  dbDisconnect(con)
-  
-  return(res)
+  query(query, schema = config$mysql$portal)
   
 }
 
@@ -67,64 +58,14 @@ portalQuery <- function(query){
 ## Function for queries to index
 indexQuery <- function(query){
   
-  con <- 
-    dbConnect(
-      MySQL(),
-      dbname = config$mysql$index,
-      host = config$mysql$host,
-      port = config$mysql$port,
-      user = config$mysql$user,
-      password = config$mysql$pass,
-      encoding = "UTF-8"
-    )
-  
-  dbSendQuery(con, "SET NAMES utf8mb4;")
-  dbSendQuery(con, "SET CHARACTER SET utf8mb4;")
-  dbSendQuery(con, "SET character_set_connection=utf8mb4;")
-  
-  req <- glue::glue_sql(query, .con = con)
-  
-  # print(req)
-  
-  req <- dbSendQuery(con, req)
-  res <- dbFetch(req, n = -1)
-  
-  dbClearResult(req)
-  
-  dbDisconnect(con)
-  
-  return(res)
+  query(query, schema = config$mysql$index)
   
 }
 
 budgetQuery <- function(query){
   
-  con <- 
-    dbConnect(
-      MySQL(),
-      dbname = config$mysql$budget,
-      host = config$mysql$host,
-      port = config$mysql$port,
-      user = config$mysql$user,
-      password = config$mysql$pass
-    )
-  
-  dbSendQuery(con, "SET NAMES utf8mb4;")
-  dbSendQuery(con, "SET CHARACTER SET utf8mb4;")
-  dbSendQuery(con, "SET character_set_connection=utf8mb4;")
-  
-  req <- glue::glue_sql(query, .con = con)
-  
-  # print(req)
-  
-  req <- dbSendQuery(con, req)
-  res <- dbFetch(req, n = -1)
-  
-  dbClearResult(req)
-  
-  dbDisconnect(con)
-  
-  return(res)
+  query(query, schema = config$mysql$budget)
   
 }
+
 
